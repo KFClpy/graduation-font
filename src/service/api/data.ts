5b3f77463d05ecdc3a41a61c4e170f575b9226de
KFClpy <2291897613@qq.com>
2023-04-16 16:23:42 +0800

    show table

--- src/service/api/data.ts
+++ src/service/api/data.ts
@@ -3,3 +3,7 @@ import { request } from '@/service/request';
 export const getDataName = async () => {
   return request.post<DataAuth.Data_name | null>('/getDataName');
 }
+
+export const getDataTable = async (data_name: string) => {
+	return request.post<object>('/getDataTable', { data_name });
+}
--- src/views/management/auth/index.vue
+++ src/views/management/auth/index.vue
@@ -3,12 +3,21 @@
     <n-card title="表格" class="h-full shadow-sm rounded-16px">
       <n-space :vertical="true">
         <n-space>
-          <n-button @click="getDataSource">有数据</n-button>
-          <n-button @click="getEmptyDataSource">空数据</n-button>
-					<n-select v-model:value="value" :options="options" :consistent-menu-width="false" @update:value="handleUpdateValue" />
+          <n-select
+            v-model:value="value"
+            :options="options"
+            :consistent-menu-width="false"
+            @update:value="handleUpdateValue"
+          />
         </n-space>
         <loading-empty-wrapper class="h-480px" :loading="loading" :empty="empty">
-          <n-data-table :columns="columns" :data="dataSource" :flex-height="true" class="h-480px" />
+          <n-data-table
+            :columns="columns"
+            :data="dataSource"
+            :flex-height="true"
+            :pagination="pagination"
+            class="h-480px"
+          />
         </loading-empty-wrapper>
       </n-space>
     </n-card>
@@ -16,38 +25,45 @@
 </template>
 
 <script setup lang="tsx">
-import { onMounted, ref } from 'vue';
-import { NSpace, NButton, NPopconfirm, SelectOption } from 'naive-ui';
-import type { DataTableColumn } from 'naive-ui';
+import { onMounted, reactive, ref } from 'vue';
+import { NSpace, NButton, NPopconfirm, PaginationProps } from 'naive-ui';
+import type { DataTableColumn, SelectOption } from 'naive-ui';
 import { useLoadingEmpty } from '@/hooks';
-import { getRandomInteger } from '@/utils';
-import { getDataName } from '@/service/api/data';
+import { getDataName, getDataTable } from '@/service/api/data';
 
-interface DataSource {
-  name: string;
-  age: number;
-  address: string;
+const { loading, startLoading, endLoading, empty } = useLoadingEmpty();
+
+let columns: DataTableColumn[] = [];
+const dataSource = ref([]);
+// 创建options，每个option都有label和value，value和option都是列表data_name_list中的值
+const options = ref([]);
+const value = ref(' 请选择数据 ');
+
+async function updateDataName() {
+  const { data } = await getDataName();
+  // value.value=data?.data_name[0];
+  // 将data_name_list.data中的值赋给options
+  options.value = data?.data_name.map(item => {
+    return {
+      label: item,
+      value: item
+    };
+  });
 }
-
-const { loading, startLoading, endLoading, empty, setEmpty } = useLoadingEmpty();
-
-const columns: DataTableColumn[] = [
-  {
-    title: 'Name',
-    key: 'name',
-    align: 'center'
-  },
-  {
-    title: 'Age',
-    key: 'age',
-    align: 'center'
-  },
-  {
-    title: 'Address',
-    key: 'address',
-    align: 'center'
-  },
-  {
+async function updateDataTable() {
+  startLoading();
+  const { data } = await getDataTable(value.value);
+  const data_header = Object.keys(data);
+  const data_value = Object.values(data);
+  columns = [];
+  columns = data_header.map(item => {
+    return {
+      title: item,
+      key: item,
+      align: 'center'
+    };
+  });
+  columns.push({
     key: 'action',
     title: 'Action',
     align: 'center',
@@ -66,65 +82,49 @@ const columns: DataTableColumn[] = [
         </NSpace>
       );
     }
-  }
-];
-
-const dataSource = ref<DataSource[]>([]);
-
-function createDataSource(): DataSource[] {
-  return Array(100)
-    .fill(1)
-    .map((_item, index) => {
-      return {
-        name: `Name${index}`,
-        age: getRandomInteger(30, 20),
-        address: '中国'
-      };
-    });
-}
-
-// 创建options，每个option都有label和value，value和option都是列表data_name_list中的值
-const options = ref([]);
-const value = ref();
-function getDataSource() {
-  startLoading();
-  setTimeout(() => {
-    dataSource.value = createDataSource();
-    endLoading();
-    setEmpty(!dataSource.value.length);
-  }, 1000);
-}
-
-async function updateDataName() {
-  const { data } = await getDataName();
-	value.value=data?.data_name[0];
-  console.log(data?.data_name);
-  // 将data_name_list.data中的值赋给options
-  options.value = data?.data_name.map(item => {
-    return {
-      label: item,
-      value: item
-    };
   });
-  console.log(options);
+  console.log(columns);
+  dataSource.value = [];
+  // data_value是一个类，内容是二维数组，他的行号是data_header的下标，列号是每个data_header的值，把每一行的值赋给data_source
+  let rowlen = 0;
+  for (let i = 0; i < data_value.length; i++) {
+    for (let j = 0; j < 1; j++) {
+      rowlen = data_value[i].length;
+    }
+  }
+  for (let i = 0; i < rowlen; i++) {
+    const data_source = {};
+    for (let j = 0; j < data_header.length; j++) {
+      data_source[data_header[j]] = data_value[j][i];
+    }
+    dataSource.value.push(data_source);
+  }
+  console.log(dataSource.value);
+  endLoading();
 }
-
-function handleUpdateValue (value: string, option: SelectOption) {
-	window?.$message?.info('value: ' + JSON.stringify(value))
-	window?.$message?.info('option: ' + JSON.stringify(option))
-}
-function getEmptyDataSource() {
-  startLoading();
-  setTimeout(() => {
-    dataSource.value = [];
-    endLoading();
-    setEmpty(!dataSource.value.length);
-  }, 1000);
+const pagination: PaginationProps = reactive({
+	page: 1,
+	pageSize: 10,
+	showSizePicker: true,
+	pageSizes: [10, 15, 20, 25, 30],
+	onChange: (page: number) => {
+		pagination.page = page;
+	},
+	onUpdatePageSize: (pageSize: number) => {
+		pagination.pageSize = pageSize;
+		pagination.page = 1;
+	}
+});
+function handleUpdateValue(value: string, option: SelectOption) {
+  // window?.$message?.info(`value: ${JSON.stringify(value)}`);
+  // window?.$message?.info(`option: ${JSON.stringify(option)}`);
+  updateDataTable();
 }
 
 onMounted(() => {
-  getDataSource();
+  // getDataSource();
   updateDataName();
+  updateDataTable();
 });
 </script>
 
