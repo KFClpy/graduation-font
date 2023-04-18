bcd08b1ac92ffcc28e067f213f39d3996074f186
KFClpy <2291897613@qq.com>
2023-04-18 21:53:29 +0800

    edit

--- src/service/api/data.ts
+++ src/service/api/data.ts
@@ -2,13 +2,13 @@ import { request } from '@/service/request';
 
 export const getDataName = async () => {
   return request.post<DataAuth.Data_name | null>('/getDataName');
-}
+};
 
 export const getDataTable = async (data_name: string) => {
-	return request.post<object>('/getDataTable', { data_name });
-}
+  return request.post<object>('/getDataTable', { data_name });
+};
 export function autoFuzzyJoin(data_name_left: string, data_name_right: string, data_name_generate: string) {
-	return request.post<ApiAuth.UserFlag>('/autoFuzzyJoin', {data_name_left, data_name_right, data_name_generate});
+  return request.post<ApiAuth.UserFlag>('/autoFuzzyJoin', { data_name_left, data_name_right, data_name_generate });
 }
 
 export function deleteOneData(tid: number) {
@@ -16,5 +16,6 @@ export function deleteOneData(tid: number) {
 }
 
 export function editOneData(tid: number, data: object) {
-	return request.post<ApiAuth.UserFlag>('/editOneData', { tid, data });
+  return request.post<ApiAuth.UserFlag>('/editOneData', { tid, data });
 }
+
